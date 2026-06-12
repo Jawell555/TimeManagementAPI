2bf018d507b46be611d66fb4c19e2f10f124ad6f
Jawell555 <sanlorenzojruell@gmail.com>
2026-06-12 10:18:04 +0800

    Fix TimeLogs HTTP GET

--- TimeMngmntAPI/Controllers/TimeLogsController.cs
+++ TimeMngmntAPI/Controllers/TimeLogsController.cs
@@ -33,6 +33,20 @@ namespace TimeMngmntAPI.Controllers
             }
             return Ok(timeLog);
         }
+        [HttpGet("{date}")]
+        public ActionResult<TimeLogs> GetLatestEmployeeLogByDate(string date)
+        {
+            if (!TimeValidationServices.TryParseValidDate(date, out DateOnly parsedDate))
+            {
+                return BadRequest("Invalid date format. Use MM/dd/yyyy (e.g., 12/31/2024).");
+            }
+            var timeLog = _appService.GetTimeLogsByDate(parsedDate);
+            if (timeLog == null)
+            {
+                return NotFound("No time log found for the specified date.");
+            }
+            return Ok(timeLog);
+        }
         [HttpGet("latest-logs")]
         public ActionResult<IEnumerable<TimeLogs>> GetLatestEmployeeLogs()
         {
@@ -50,20 +64,6 @@ namespace TimeMngmntAPI.Controllers
             if (timeLog == null)
             {
                 return NotFound("No time log found for the specified employee ID.");
-            }
-            return Ok(timeLog);
-        }
-        [HttpGet("latest-logs/{date:string}")]
-        public ActionResult<TimeLogs> GetLatestEmployeeLogByDate(string date)
-        {
-            if (!TimeValidationServices.TryParseValidDate(date, out DateOnly parsedDate))
-            {
-                return BadRequest("Invalid date format. Use MM/dd/yyyy (e.g., 12/31/2024).");
-            }
-            var timeLog = _appService.GetTimeLogsByDate(parsedDate);
-            if (timeLog == null)
-            {
-                return NotFound("No time log found for the specified date.");
             }
             return Ok(timeLog);
         }
